6e6e8bf72d0281aa67375feea00f5be45e1406db
Brian Nduhiu <briannduhiu@gmail.com>
2021-06-16 14:03:49 +0300

    chapter 3 progress

--- main.py
+++ main.py
@@ -38,3 +38,66 @@
 
 # name = input("Enter your name: ")
 # print(f"Welcome {name.upper()}")
+
+
+# import random
+
+# die1 = random.randint(1, 6)
+# die2 = random.randrange(6)+1
+
+# print(f"You have selected {die1} and {die2}")
+
+# print(f"Welcome to the login simulator \n")
+# user_key = input("Please enter your password: ")
+
+# if user_key == 'secret':
+#     print(f"access granted \n")
+# else:
+#     print(f"access denied \n")
+
+
+# import random
+# print("I sense your energy.  Your true emotions are coming across my screen.")
+# print("You are...")
+# mood = random.randint(1, 3)
+
+# if mood == 1:
+#     # happy
+#     print("""
+#     -----------
+#     |         |
+#     | O    O  |
+#     |   <     |
+#     |         |
+#     | .     . |
+#     |  `...`  |
+#     ----------- """)
+
+# elif mood == 2:
+#     # neutral
+#     print("""
+#      ------------
+#      |          |
+#      |  O    O  |
+#      |    <     |
+#      |          |
+#      |  ------  |
+#      |          |
+#      ------------""")
+
+# elif mood == 3:
+#     # sad
+#     print("""
+#     -----------
+#     |          |
+#     |  O    O  |
+#     |    <     |
+#     |          |
+#     |   .'.    |
+#     |  '   '   |
+#      ----------- """)
+
+# else:
+#     print("Illegal mood value!  (You must be in a really bad mood).")
+
+# print("...today.")
