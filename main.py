c29f351b78a7e85289072357c394ffb383949a1d
Brian Nduhiu <briannduhiu@gmail.com>
2021-06-18 15:15:08 +0300

    Added documentation to the main.py file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#  This file contains some of the trial code I use to test various concepts in python
 
+
+# Printing the word Game Over with the use of symbols
 # print("""
 #      ______
 #     |
@@ -21,41 +24,46 @@
 #       | |_| | | |/ /    | |___  | | \ \
 #       \_____/ |___/     |_____| |_|  \_\ """)
 
-
+# Use of the 'end' parameter in print to specify what I want to be the last character in my print statement
 # print("This is", end=" : ")
 # print("Nairobi")
 
+# Using double forward slashes to print out a forward slash
 # print("\\")
 
+
+# Using a forward slash to break a statement into two lines without causing an error
 # if 1 > \
 #         2:
 #     print("one is greater than two")
 # else:
 #     print("two is greater than one")
 
+# Printing the difference between the arithmetic operator / and //
 # print(7/2)
 # print(7//2)
 
+# Use of Input and converting the string to uppercase letters
 # name = input("Enter your name: ")
 # print(f"Welcome {name.upper()}")
 
-
+# Importing the random class and using randint and randrange to generate random numbers
 # import random
-
 # die1 = random.randint(1, 6)
 # die2 = random.randrange(6)+1
-
 # print(f"You have selected {die1} and {die2}")
 
+
+# A login simulator that simulates a login page where the password is secret
 # print(f"Welcome to the login simulator \n")
 # user_key = input("Please enter your password: ")
-
 # if user_key == 'secret':
 #     print(f"access granted \n")
 # else:
 #     print(f"access denied \n")
 
 
+# A program that tries to detect and show you your mood based on the random numbers it generates
 # import random
 # print("I sense your energy.  Your true emotions are coming across my screen.")
 # print("You are...")
