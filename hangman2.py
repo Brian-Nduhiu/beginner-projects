81a3d3d0e4947122cbab5fe998054c12e56bfd98
Brian Nduhiu <briannduhiu@gmail.com>
2021-06-29 19:33:21 +0300

    Çleared the infinite loop in hangman2

--- hangman2.py
+++ hangman2.py
@@ -120,34 +120,34 @@ while wrong < MAX_WRONG and so_far != word:
     print("\nYou've used the following letters:\n", used)
     print("\nSo far, the word is:\n", so_far)
 
-guess = input("\n\nEnter your guess: ")
-guess = guess.upper()
-while guess in used:
-    print("You've already guessed the letter", guess)
-    guess = input("Enter your guess: ")
+    guess = input("\n\nEnter your guess: ")
     guess = guess.upper()
+    while guess in used:
+        print("You've already guessed the letter", guess)
+        guess = input("Enter your guess: ")
+        guess = guess.upper()
 
-used.append(guess)
+    used.append(guess)
 
-if guess in word:
-    print("\nYes!", guess, "is in the word!")
+    if guess in word:
+        print("\nYes!", guess, "is in the word!")
 
-    # create a new so_far to include guess
-    new = ""
-    for i in range(len(word)):
-        if guess == word[i]:
-            new += guess
-        else:
-            new += so_far[i]
-            so_far = new
+        # create a new so_far to include guess
+        new = ""
+        for i in range(len(word)):
+            if guess == word[i]:
+                new += guess
+            else:
+                new += so_far[i]
+                so_far = new
 
-else:
-    print("\nSorry,", guess, "isn't in the word.")
-    wrong += 1
+    else:
+        print("\nSorry,", guess, "isn't in the word.")
+        wrong += 1
 
-if wrong == MAX_WRONG:
-    print(HANGMAN[wrong])
-    print("\nYou've been hanged!")
-else:
-    print("\nYou guessed it!")
-    print("\nThe word was", word)
+    if wrong == MAX_WRONG:
+        print(HANGMAN[wrong])
+        print("\nYou've been hanged!")
+    else:
+        print("\nYou guessed it!")
+        print("\nThe word was", word)
