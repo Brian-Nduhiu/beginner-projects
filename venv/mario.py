b9d89e4e07d90285cc55e788013fd78b579cc83f
Brian Nduhiu <briannduhiu@gmail.com>
2021-08-27 11:44:47 +0300

    Left sided pyramid

--- venv/mario.py
+++ venv/mario.py
@@ -2,9 +2,28 @@
 
 
 # Right sided pyramid
+
+#
+##
+###
+####
+
+
 for i in range(10):
     for j in range(i):
         print(end=" " "#")
     print("\n")
 
 # Left sided pyramid
+    #
+   ##
+  ###
+ ####
+#####
+
+for i2 in range(10):
+    for j2 in range(10-i2):
+        print(end=" " " ")
+    for j22 in range(i2):
+        print(end=" " "#")
+    print("\n")
