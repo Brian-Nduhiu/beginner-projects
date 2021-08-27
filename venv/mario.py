4702e2048b4a188c1dd5ca9f21c96c15c52ba20f
Brian Nduhiu <briannduhiu@gmail.com>
2021-08-27 12:14:24 +0300

    half hour glass

--- venv/mario.py
+++ venv/mario.py
@@ -27,3 +27,33 @@ for i2 in range(10):
     for j22 in range(i2):
         print(end=" " "#")
     print("\n")
+
+print("\n\n")
+# Half pyramid
+
+######
+#####
+####
+###
+##
+#
+#
+##
+###
+####
+#####
+######
+
+
+for i3 in range(10):
+    for j3 in range(i3):
+        print(end=" " " ")
+    for j32 in range(10-i3):
+        print(end=" " "#")
+    print("\n")
+for i4 in range(10):
+    for j4 in range(10-i4):
+        print(end=" " " ")
+    for j42 in range(i4):
+        print(end=" " "#")
+    print("\n")
