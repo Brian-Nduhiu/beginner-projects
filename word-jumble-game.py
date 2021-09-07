0b9215da386440d80f7b51edfca77d60c9c85a7f
Brian <briannduhiu@gmail.com>
2021-09-07 14:56:53 +0300

    completed word jumble game

--- word-jumble-game.py
+++ word-jumble-game.py
@@ -8,5 +8,34 @@ randomWord = words[random.randint(0,len(words) - 1)]
 
 jumbledWord = ""
 
-while len(jumbledWord) != len(randomWord):
+indexlist = []
+
+for i in range(len(randomWord)):
+    indexlist.append(i)
+
+# print(indexlist)
+
+randomlist = []
+
+while len(randomlist) != len(indexlist):
+    randnum = random.randint(0, len(indexlist)-1)
+    if randnum not in randomlist:
+        randomlist.append(randnum)
     
+# print(randomlist)
+
+for i in randomlist:
+    jumbledWord += randomWord[i]
+
+print(f"The Jumbled Word is {jumbledWord}")
+
+guess = input("Guess the word: ")
+
+if guess == randomWord:
+    print(f"Correct the random word is {randomWord}")
+
+else:
+    print(f"Sorry, the random word is {randomWord}")
+
+
+
