1a979b73db92cccd60a14ffd83352cba6b6d1227
Brian <briannduhiu@gmail.com>
2021-09-14 16:51:59 +0300

    completed fizzbuzz challenge

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,13 @@
 # But for multiples of three print "Fizz" instead of the number
 # and for the multiples of five print "Buzz".
 # For numbers that are multiples of both three and five print "FizzBuzz".
+
+for num in range(100):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
